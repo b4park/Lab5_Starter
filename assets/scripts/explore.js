39d83c6269f11e0a530f180e0e80f8c067b3891a
b4park <57046049+b4park@users.noreply.github.com>
2021-10-23 12:34:26 -0700

    Add files via upload

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,38 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const voices = window.speechSynthesis;
 
 function init() {
   // TODO
+  const talkButton = document.querySelector('button');
+  const words = document.getElementById('text-to-speak');
+  const voiceList = document.getElementById('voice-select');
+  const talk = new SpeechSynthesisUtterance(words.value);
+  //let smile = document.querySelector(‘img’);
+  let ttsVoices;
+  setTimeout(() => {
+    ttsVoices = voices.getVoices();
+    talk.voice = ttsVoices[0];
+    ttsVoices.forEach((voice, i) => voiceList.options[i] = new Option(voice.name, i));
+  }, 100);
+  talkButton.addEventListener('click', () =>
+  {
+    const talk = new SpeechSynthesisUtterance(words.value);
+    document.querySelector('img').src = 'assets/images/smiling-open.png';
+    talk.voice = ttsVoices[document.getElementById('voice-select').value];
+    console.log(document.getElementById('voice-select').value);
+    speechSynthesis.speak(talk);
+    for(var i = 0; i < 1; i++)
+    {
+      setInterval(function()
+      {
+        if(speechSynthesis.speaking == false)
+        {
+          document.querySelector('img').src = 'assets/images/smiling.png';
+        }
+      }, 1);
+      break;
+    }
+  })
 }
