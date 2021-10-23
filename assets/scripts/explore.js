27ce3d63d7006339327d73bf53f3ee2a77903f72
Hugs Clorina <60804033+htclorina@users.noreply.github.com>
2021-10-23 15:27:30 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,7 +3,7 @@
 window.addEventListener('DOMContentLoaded', init);
 const voices = window.speechSynthesis;
 
-function init() {
+/*function init() {
   // TODO
   const talkButton = document.querySelector('button');
   const words = document.getElementById('text-to-speak');
@@ -17,6 +17,40 @@ function init() {
     ttsVoices.forEach((voice, i) => voiceList.options[i] = new Option(voice.name, i));
   }, 100);
   talkButton.addEventListener('click', () =>
+  {
+    const talk = new SpeechSynthesisUtterance(words.value);
+    document.querySelector('img').src = 'assets/images/smiling-open.png';
+    talk.voice = ttsVoices[document.getElementById('voice-select').value];
+    console.log(document.getElementById('voice-select').value);
+    speechSynthesis.speak(talk);
+    for(var i = 0; i < 1; i++)
+    {
+      setInterval(function()
+      {
+        if(speechSynthesis.speaking == false)
+        {
+          document.querySelector('img').src = 'assets/images/smiling.png';
+        }
+      }, 1);
+      break;
+    }
+  })
+}*/
+function init() {
+  const talkButton = document.querySelector('button');
+  const words = document.getElementById('text-to-speak');
+  const voiceList = document.getElementById('voice-select');
+  //const talk = new SpeechSynthesisUtterance(words.value);
+  //let smile = document.querySelector('img');
+  let ttsVoices;
+  setTimeout(() => {
+    ttsVoices = voices.getVoices();
+    for(var i = 0; i < ttsVoices.length; i++)
+    {
+      voiceList.options[i] = new Option(ttsVoices[i].name, i);
+    }
+  }, 100);
+  talkButton.addEventListener('click', () => 
   {
     const talk = new SpeechSynthesisUtterance(words.value);
     document.querySelector('img').src = 'assets/images/smiling-open.png';
